8e492c80b6426ecefecd6c31871eb402cae01fb9
Santino Zanone <46961279@comunidadort.edu.ar>
2026-06-15 23:18:59 -0300

    feat: añadi PWA

--- taskflow-frontend/vite.config.js
+++ taskflow-frontend/vite.config.js
@@ -1,9 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+     VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'TaskFlow',
+        short_name: 'TaskFlow',
+        description: 'Aplicación de gestión de tareas.',
+        theme_color: '#ffffff',
+
+        icons: [
+          {
+            src: 'pwa-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
+  ],
   resolve: {
     alias: {
       '@': '/src',
